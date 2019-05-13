b5b9f739d35da742d5f653c6317cba624b6d9d15
williams osunkwo <wosunkwo@gmail.com>
2019-05-13 13:43:26 -0700

    set up the server.js dependencies

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
