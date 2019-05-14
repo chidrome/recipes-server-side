3dffaaa8eb9accc51fa5cbaff9eca693dddec863
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-14 11:13:19 -0700

    prevented duplicates, created contraint for unique entries

--- data/schema.sql
+++ data/schema.sql
@@ -1,23 +1,15 @@
 DROP table if exists recipes;
-/*
-label = recipe.label;
-    this.image = recipe.image;
-    this.yield = recipe.yield;
-    this.calories = Math.ceil(recipe.calories);
-    this.totalTime = recipe.totalTime;
-    this.ingredients = recipe.ingredients;
-    this.dietLabels = recipe.dietLabels;
-    this.healthLabels = recipe.healthLabels;
-    recipeResults.push(this);
-*/
+
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   label VARCHAR(100),
-  image VARCHAR(200),
+  image VARCHAR(200) UNIQUE,
+  url VARCHAR(200) UNIQUE,
   yield INTEGER,
   calories INTEGER,
   total_time INTEGER,
   ingredients text[],
   diet_labels text[],
-  health_labels text[]
-)
+  health_labels text[],
+  CONSTRAINT url_unique UNIQUE (url)
+);
