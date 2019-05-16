a65dd6dede670ba6b8e0758d12b3420ed337ced9
chidrome <chidrome@gmail.com>
2019-05-16 10:31:53 -0700

    password is now hashed when signing up and login can compare the hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const express = require('express');
 const pg = require('pg');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 //Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -24,14 +25,19 @@ router.post('/login', (req, res) => {
                 return res.status(400).send('User not found');
             }
             // user found, check password
-            if(user.rows[0].password === req.body.password){
-                // valid user passed authentication
-                const token = jwt.sign(user.rows[0], process.env.JWT_SECRET, {
-                    expiresIn: 60 * 60 * 24 // 24 hours (in seconds)
-                });
-                res.send({ token: token });
-                return res.status(401).send('Invalid Credentials');
-            }
+            bcrypt.compare(req.body.password, user.rows[0].password, (error, passwordsMatch) => {
+                if(passwordsMatch){
+                    const token = jwt.sign(user.rows[0], process.env.JWT_SECRET, {
+                        expiresIn: 60 * 60 * 24 // 24 hours (in seconds)
+                    });
+                    res.send({ token: token });
+                } else {
+                    return res.status(401).send('Invalid Credentials');
+                }
+                if(error){
+                    console.log(`Error comparing password. ${error}`);
+                }
+            });
         });
 
 });
@@ -43,18 +49,28 @@ router.post('/signup', (req, res) => {
     client.query(SearchEmail)
         .then(results => {
             if(results.rowCount === 0){
-                const SQL = 'INSERT into users (name, email, password) VALUES($1, $2, $3) ON CONFLICT DO NOTHING;';
-                const values = [req.body.name.toLowerCase(), req.body.email.toLowerCase(), req.body.password];
-                client.query(SQL, values)
-                    .then(results => {
-                        const token = jwt.sign(results.rows[0], process.env.JWT_SECRET, {
-                            expiresIn: 60 * 60 * 24 // 24 hours (in seconds)
+                bcrypt.hash(req.body.password, 10, (error, hash) => {
+                    const SQL = 'INSERT into users (name, email, password) VALUES($1, $2, $3) ON CONFLICT DO NOTHING;';
+                    const values = [req.body.name.toLowerCase(), req.body.email.toLowerCase(), hash];
+                    client.query(SQL, values)
+                        .then(results => {
+                            console.log('Hi');
+                            console.log(results);
+                            const token = jwt.sign(results.rows[0], process.env.JWT_SECRET, {
+                                expiresIn: 60 * 60 * 24 // 24 hours (in seconds)
+                            });
+                            res.send({ token: token });
+                            console.log(`this is your token ${token}`);
+
+                        })
+                        .catch(error => {
+                            console.log(`error signing up ${error}`);
                         });
-                        res.send({ token: token });
-                    })
-                    .catch(error => {
-                        console.log(`error signing up ${error}`);
-                    });
+                    if(error){
+                        console.log(`There's been an error with hashing your password! ${error}`);
+                    }
+                });
+                console.log(`results after the bcrypt------------- ${results}`);
             } else if(results.rowCount === 1){
                 // created a new user. Yay! Time to send a token for them!
                 const token = jwt.sign(results.rows[0], process.env.JWT_SECRET, {
